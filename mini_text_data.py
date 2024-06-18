2f75a593e82fd429a4f5309bbfadb9628ac06b3b
Tensor <narutokelechi@gmail.com>
2024-06-18 10:17:12 +0100

    text tokenization code for text file

---
+++ mini_text_data.py
@@ -0,0 +1,28 @@
+import os, requests
+import numpy as np
+import tiktoken
+
+file_path = 'research.txt'
+
+with open(file_path, 'r', encoding='utf-8') as file_write:
+    text_data = file_write.read()
+    
+data_len = len(text_data)
+train_size = 0.9 * data_len
+
+train_data = text_data[:train_size]
+val_data = text_data[train_size:]
+
+# tiktoken encoding
+encoder = tiktoken.get_encoding('gpt2')
+train_token_ids = encoder.encode_ordinary(train_data)
+val_token_ids = encoder.encode_ordinary(val_data)
+
+print(f'train set => {len(train_token_ids):,} \n val set => {len(val_token_ids):,}')
+
+# export to binary
+train_token_ids = np.array(train_token_ids, dtype=np.uint16)
+val_token_ids = np.array(val_token_ids, dtype=np.uint16)
+
+train_token_ids.tofile('train_tokens.bin')
+val_token_ids.tofile('val_tokens.bin')
--- text_data.py
+++ text_data.py
@@ -1,2 +1,7 @@
 import os, re
 import datasets 
+import tiktoken
+import numpy as np
+from datasets import load_dataset
+
+open_webtext = load_dataset('')
