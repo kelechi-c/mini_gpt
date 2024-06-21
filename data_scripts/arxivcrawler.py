403a0456b6d89f065f142de0a5a3aba12456b6b5
Tensor <narutokelechi@gmail.com>
2024-06-21 05:53:12 +0100

    script to retrieve entire cs research

--- data_scripts/arxivcrawler.py
+++ data_scripts/arxivcrawler.py
@@ -6,46 +6,33 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
-section = "CV"  # LG(machine learning), CL(computation and language)
-pdf_folder = "arxiv_cs_{section}"
-os.mkdir(pdf_folder)
 
-arxiv_url = "http://arxiv.org/"
-arxiv_tree_url = f"http://arxiv.org/list/cs.{section}"
+def get_section_list():  # get list of sections 
+    # initialize driver
+    drive_opts = Options()
+    drive_opts.add_argument("-headless")
+    driver = webdriver.Firefox(options=drive_opts)
+    print("Driver init")
 
-# initialize driver
-drive_opts = Options()
-drive_opts.add_argument("-headless")
-driver = webdriver.Firefox(options=drive_opts)
-print("Driver init")
+    # load page content
+    driver.get("https://arxiv.org/")
 
-# load page content
-page = driver.get(f"{arxiv_tree_url}/recent?skip=0&show=2000")
-print("Driver loaded...🔥")
+    links = driver.find_elements(By.CSS_SELECTOR, "li > a")
+    links = links[4:]
 
-# get all sections in computer science
-cs_sections = driver.find_elements(By.ID, "cs")
-section_acronyms = [link.get_attribute("id") for link in cs_sections]
-section_acronyms = [sect.upper() for sect in section_acronyms]  # type: ignore
+    print(len(links))
 
+    field_acronyms = [link.get_attribute('id') for link in links]
 
-# get all pdf links and titles
-section_links = driver.find_elements(By.ID, f"cs.{section}")
-titles = driver.find_elements(By.CLASS_NAME, "list-title.mathjax")
-print(f"Number of links  => {len(section_links)}")
+    field_acronyms = [k for k in field_acronyms if k.startswith("cs")]
+    driver.quit()
 
-
-pdf_links = [link.get_attribute("src") for link in section_links]
-paper_titles = [tit.text for tit in titles]
-print(len(pdf_links), len(paper_titles))
-print(f"Link and titles retrieved. e.g => {paper_titles[0]: pdf_links[0]}")
-
-# quit driver instance
-driver.quit()
+    print(field_acronyms)
+    return field_acronyms
 
 
 # download files wuth corresponding paper titles
-def download_files(link_list: list, title_list: list):
+def download_files(link_list: list, title_list: list, pdf_folder: str):
     k = 0
 
     for file_link, title in tqdm(zip(link_list, title_list)):
@@ -66,15 +53,6 @@ def download_files(link_list: list, title_list: list):
             continue
 
     print(f"{k} pdfs downloaded from arxiv 🍻")
-
-
-download_files(pdf_links, paper_titles)
-
-
-####
-# for merging all the pdfs into a single text file
-
-print("Begin pdf merging")
 
 
 def clean_text(text):  # regex cleaning function
@@ -118,23 +96,25 @@ def merge_pdfs(file_list: list, out_text_file: str):
 
         # success message
         rich.print(
-            f"{len(pdf_list)} research paper PDFs extracted to single text file [bold green]{out_text_file}[/bold green] of size {shutil.disk_usage(out_text_file)}"
+            f"{len(file_list)} research paper PDFs extracted to single text file [bold green]{out_text_file}[/bold green] of size {shutil.disk_usage(out_text_file)}"
         )
 
         return text_corpus
+    
     # exception hanndling and colored output
     except Exception as e:
         rich.print(f"[bold red] Error in extraction --> {e}")
 
+    return text_corpus
 
 
- 
 def arxiv_scraper(section: str):
     arxiv_tree_url = f"http://arxiv.org/list/cs.{section}"
 
-    section = "PL"  # LG(machine learning), CL(computation and language)
+     # LG(machine learning), CL(computation and language)
     pdf_folder = f"arxiv_cs_{section}"
     os.mkdir(pdf_folder)
+
 
     # initialize driver
     drive_opts = Options()
@@ -142,7 +122,7 @@ def arxiv_scraper(section: str):
     driver = webdriver.Firefox(options=drive_opts)
     print("Driver init")
 
-    # load page content
+     # load page content
     driver.get(f"{arxiv_tree_url}/recent?skip=0&show=2000")
     print("Driver loaded...🔥")
 
@@ -161,10 +141,26 @@ def arxiv_scraper(section: str):
     driver.quit()
     print('driver terminated')
 
+    download_files(pdf_links, paper_titles, pdf_folder)
+
+
     print("Begin pdf merging")
     pdf_list = os.listdir(pdf_folder)
     pdf_list = [os.path.join(os.getcwd(), pdf_folder, pdfile) for pdfile in pdf_list]
 
     out_text_file = f"arxiv_cspapers_{section}.txt"
-    merge_pdfs(pdf_list, out_text_file)
+    
+    corpus = merge_pdfs(pdf_list, out_text_file)
     print("Extraction and merging complete ⚡️⚡️")
+
+    return corpus
+
+
+# main
+cs_fields = get_section_list()
+
+for cs_field in cs_fields:
+    arxiv_corpus = arxiv_scraper(cs_field)
+    print(f'total length of corpus => {len(arxiv_corpus)}')
+
+print('Arxiv computer science research text data successfully scraped')
